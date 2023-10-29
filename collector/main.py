5f34ede1e302135248164fea6ed403f2992203b1
gorcajo <guillermo.orcajo@gmail.com>
2023-10-29 17:42:43 +0100

    collector: read data

--- collector/main.py
+++ collector/main.py
@@ -1,5 +1,23 @@
-import json
+# See <https://www.bedca.net/bdpub/>
 
+
+import json
+import logging
+import time
+
+import bs4
+import requests
+
+
+BASE_URL = 'https://www.bedca.net/bdpub/procquery.php'
+REQUEST_TEMPLATES_DIR_PATH = './request-templates'
+LIST_TEMPLATE_BODY_PATH = f'{REQUEST_TEMPLATES_DIR_PATH}/list.xml'
+DETAIL_TEMPLATE_BODY_PATH = f'{REQUEST_TEMPLATES_DIR_PATH}/detail.xml'
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+CALORIES_EUR_NAME = 'ENERC'
+FAT_EUR_NAME = 'FAT'
+PROTEIN_EUR_NAME = 'PROT'
+CARBOHYDRATE_EUR_NAME = 'CHO'
 
 OUTPUT_DIR_PATH = '../data'
 OUTPUT_CSV_PATH = f'{OUTPUT_DIR_PATH}/data.csv'
@@ -10,27 +28,114 @@ NutritionFacts = dict[str, str | float]
 
 
 def main() -> None:
+    logging.info(f'Started')
+    start_seconds: float = time.time()
+
     data: list[NutritionFacts] = read_data()
     write_data(data)
 
+    elapsed_seconds: float = time.time() - start_seconds
+    logging.info(f'Done! Elapsed: {elapsed_seconds:.1f} s')
+
 
 def read_data() -> list[NutritionFacts]:
-    return [
-        {
-            'name': 'hard-boiled egg',
-            'calories': 131.0,
-            'fat': 9.0,
-            'carbohydrate': 0.0,
-            'protein': 12.6,
-        },
-        {
-            'name': 'apple',
-            'calories': 52.0,
-            'fat': 0.2,
-            'carbohydrate': 14.0,
-            'protein': 0.3,
-        }
-    ]
+    logging.info('Reading request body templates')
+    list_template_body_template: str = read_request_template(LIST_TEMPLATE_BODY_PATH)
+    detail_template_body_template: str = read_request_template(DETAIL_TEMPLATE_BODY_PATH)
+
+    food_ids_and_names: list[tuple[int, str]] = get_all_food_ids_and_names(list_template_body_template)
+
+    all_nutrition_facts: list[NutritionFacts] = []
+
+    for food_id, food_name in food_ids_and_names:
+        nutrition_facts: NutritionFacts | None = get_nutrition_facts_for_food_id(detail_template_body_template, food_id, food_name)
+
+        if nutrition_facts is None:
+            continue
+        else:
+            all_nutrition_facts.append(nutrition_facts)
+
+    return all_nutrition_facts
+
+
+def read_request_template(request_template_path: str) -> str:
+    with open(request_template_path) as list_request_template_file:
+        return list_request_template_file.read()
+
+
+def get_all_food_ids_and_names(request_body_template: str) -> list[tuple[int, str]]:
+    food_ids_and_names: list[tuple[int, str]] = []
+
+    for letter in ALPHABET:
+        logging.info(f'Getting foods starting by {letter.upper()}')
+
+        response = requests.post(
+            headers = {'Content-Type': 'text/xml'},
+            data = request_body_template.replace('${letter}', letter),
+            url = BASE_URL)
+
+        if response.status_code != 200:
+            raise Exception(f'Received HTTP {response.status_code}')
+
+        soup = bs4.BeautifulSoup(response.text, features='xml')
+        foods: bs4.element.ResultSet = soup.find_all('foodresponse')[0].find_all('food')
+        
+        for food in foods:
+            id: int = int(food.find('f_id').string)
+            name: str = str(food.find('f_ori_name').string)
+            food_ids_and_names.append((id, name))
+
+    if len(food_ids_and_names) != len(set(food_ids_and_names)):
+        raise Exception('There are duplicated food IDs')
+
+    return food_ids_and_names
+
+
+def get_nutrition_facts_for_food_id(request_body_template: str, food_id: int, food_name: str) -> NutritionFacts | None:
+    logging.info(f'Getting nutrition facts for food "{food_name}"')
+
+    response = requests.post(
+        headers = {'Content-Type': 'text/xml'},
+        data = request_body_template.replace('${id}', str(food_id)),
+        url = BASE_URL)
+
+    if response.status_code != 200:
+        raise Exception(f'Received HTTP {response.status_code}')
+
+    soup = bs4.BeautifulSoup(response.text, features='xml')
+    food = soup.find_all('foodresponse')[0].find('food')
+
+    if food.find('f_ori_name') is None:
+        return None
+
+    name: str = food.find('f_ori_name').string
+    calories: float = 0.0
+    fat: float = 0.0
+    carbohydrate: float = 0.0
+    protein: float = 0.0
+
+    for foodvalue in food.find_all('foodvalue'):
+        eur_name: str = str(foodvalue.find('eur_name').string)
+        value: str = foodvalue.find('best_location').string
+
+        if value is None:
+            continue
+        elif eur_name == CALORIES_EUR_NAME:
+            calories = round(float(value) / 4.184, 2)
+        elif eur_name == FAT_EUR_NAME:
+            fat = float(value)
+        elif eur_name == PROTEIN_EUR_NAME:
+            carbohydrate = float(value)
+        elif eur_name == CARBOHYDRATE_EUR_NAME:
+            protein = float(value)
+
+    return {
+        'name': name,
+        'calories': calories,
+        'fat': fat,
+        'carbohydrate': carbohydrate,
+        'protein': protein,
+    }
 
 
 def write_data(data: list[NutritionFacts]) -> None:
@@ -38,14 +143,19 @@ def write_data(data: list[NutritionFacts]) -> None:
         output_json_file.write(json.dumps(data, separators=(',', ':')))
 
     with open(OUTPUT_CSV_PATH, 'w') as output_csv_file:
-        output_csv_file.write(",".join(data[0].keys()) + '\n')
+        output_csv_file.write(';'.join(data[0].keys()) + '\n')
 
         for i, element in enumerate(data):
-            output_csv_file.write(",".join([str(value) for value in element.values()]))
+            output_csv_file.write(';'.join([str(value) for value in element.values()]))
 
             if i < len(data) - 1:
                 output_csv_file.write('\n')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format  = '%(asctime)-5s.%(msecs)03d | %(levelname)-7s | %(funcName)s | %(message)s',
+        level   = logging.INFO,
+        datefmt = '%Y-%m-%d %H:%M:%S')
+
     main()
